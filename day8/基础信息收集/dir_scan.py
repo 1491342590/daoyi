0300ca7e5f2d73203c5ce04542fe7ebc788fb281
小蜗牛 <1491342590@qq.com>
2025-07-18 10:47:05 +0800

    7.18_1

--- day8/基础信息收集/dir_scan.py
+++ day8/基础信息收集/dir_scan.py
@@ -1,4 +1,9 @@
-
-
 class DirScan():
-    pass
+    def run(self, url, dir_dict):
+        dics = open(dir_dict, 'r').readlines()
+        for dic in dics:
+            print(dic)
+
+
+dir_scan = DirScan()
+dir_scan.run()
